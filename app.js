c2e1bc94d472563bf8c27101e685b6cf9840370e
Noah Giesel <noahgiesel@gmail.com>
2019-05-16 13:33:39 +0200

    setted the first landing page

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var apiRequest = new XMLHttpRequest();
 
 const title = document.querySelector('#title')
 const landingIMG = document.querySelector('#landingImage')
+const hdimage = document.querySelector('#hdImage')
+var thisIsTheHDImage;
+var thisIsTheNORMALImage;
 
 apiRequest.open('GET', 'https://api.nasa.gov/planetary/apod?api_key=H3LLqaHsYFliawvkIED0tmeqfMxcR6ISjTw79is1')
 
@@ -12,8 +15,10 @@ apiRequest.onload = function () {
          
           console.log(data.title)
           title.innerHTML = data.title;
-          landingIMG.src = data.hdurl;
+          landingIMG.src = data.url;
+          thisIsTheNORMALImage = data.url;
           console.log(landingIMG)
+          thisIsTheHDImage = data.hdurl;
          
       } else {
         console.log('error')
@@ -24,3 +29,12 @@ apiRequest.onload = function () {
 
 
 apiRequest.send();
+
+
+function hdImageOnHover() {
+    landingIMG.src = thisIsTheHDImage;
+}
+ 
+function normalImageOnLeave() { 
+    landingIMG.src = thisIsTheNORMALImage;
+}
