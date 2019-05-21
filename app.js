c2a0b0392eccb6d9bbe1971a78359b2c2cce9c28
Noah Giesel <noahgiesel@gmail.com>
2019-05-21 12:14:29 +0200

    added Fetch api

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-var apiRequest = new XMLHttpRequest();
-var apiRequest2 = new XMLHttpRequest();
-var apiRequest3 = new XMLHttpRequest();
+ 
 
 const mars_first = document.querySelector('#mars_first')
 const mars_first_h5 = document.querySelector('#mars_first_h5')
@@ -35,99 +33,74 @@ const description = document.querySelector('#description')
 var thisIsTheHDImage;
  var thisIsTheNORMALImage;
 
-apiRequest.open('GET', 'https://api.nasa.gov/planetary/apod?api_key=H3LLqaHsYFliawvkIED0tmeqfMxcR6ISjTw79is1')
-
-apiRequest.onload = function () {
-    var data = JSON.parse(this.response)
-
-    if (apiRequest.status >= 200 && apiRequest.status < 400) {
-         
-        title.innerHTML = "Image name : " +data.title;
-        description.innerHTML =data.explanation;
-        landingIMG.src = data.url;
-        thisIsTheNORMALImage = data.url;
-         thisIsTheHDImage = data.hdurl; 
-
-         
-      } else {
-        console.log('error mars image request problem')
-      }
-     
-
-}
-
-
-apiRequest.send();
-
-
-function hdImageOnHover() {
-    landingIMG.src = thisIsTheHDImage;
-}
  
+
+ function hdImageOnHover() {
+  landingIMG.src = thisIsTheHDImage;
+}
+
 function normalImageOnLeave() { 
-    landingIMG.src = thisIsTheNORMALImage;
+  landingIMG.src = thisIsTheNORMALImage;
 }
 
 
+fetch('https://api.nasa.gov/planetary/apod?api_key=H3LLqaHsYFliawvkIED0tmeqfMxcR6ISjTw79is1')
+  .then((response) => {
+    if(!response.ok) throw new Error(response.status);
+    else return response.json();
+  })
+  .then((data) => {
+    title.innerHTML = "Image name : " +data.title;
+    description.innerHTML =data.explanation;
+    landingIMG.src = data.url;
+    thisIsTheNORMALImage = data.url;
+     thisIsTheHDImage = data.hdurl; 
+  })
+  .catch((error) => {
+    console.log('error: ' + error); 
+  });
 
+  
    
-apiRequest2.open('GET', 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=H3LLqaHsYFliawvkIED0tmeqfMxcR6ISjTw79is1')
+ 
 
-apiRequest2.onload = function () {
-    var data = JSON.parse(this.response)
-
-    if (apiRequest2.status >= 200 && apiRequest2.status < 400) {
-         
-      mars_first.src = data.photos[0].img_src;
-      mars_first_p.innerHTML =   "launch date : "+data.photos[0].rover.launch_date;
-      mars_first_p2.innerHTML =   "landing date : " + data.photos[0].rover.landing_date; 
-      mars_first_status.innerHTML =   "status : " + data.photos[0].rover.status;
-      
-      
-      mars_second.src = data.photos[785].img_src;
-      mars_second_p.innerHTML =   "launch date : "+data.photos[785].rover.launch_date;
-      mars_second_p2.innerHTML =   "landing date : " + data.photos[785].rover.landing_date; 
-      mars_second_status.innerHTML =   "status : " + data.photos[785].rover.status; 
-             
-      } else {
-        console.log('error second mars image request')
-      }
-     
-
-} 
-
-apiRequest2.send();
-
-
+fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=H3LLqaHsYFliawvkIED0tmeqfMxcR6ISjTw79is1')
+  .then((response) => {
+    if(!response.ok) throw new Error(response.status);
+    else return response.json();
+  })
+  .then((data) => { 
+    mars_first.src = data.photos[0].img_src;
+    mars_first_p.innerHTML =   "launch date : "+data.photos[0].rover.launch_date;
+    mars_first_p2.innerHTML =   "landing date : " + data.photos[0].rover.landing_date; 
+    mars_first_status.innerHTML =   "status : " + data.photos[0].rover.status; 
+    
+    mars_second.src = data.photos[785].img_src;
+    mars_second_p.innerHTML =   "launch date : "+data.photos[785].rover.launch_date;
+    mars_second_p2.innerHTML =   "landing date : " + data.photos[785].rover.landing_date; 
+    mars_second_status.innerHTML =   "status : " + data.photos[785].rover.status; 
+  })
+  .catch((error) => {
+    console.log('error: ' + error); 
+  });
 
 
 //form section (earth) 
  
-
 function checked() { 
-   apiRequest3.open('GET', "https://api.nasa.gov/planetary/earth/imagery?lon="+longitude.value+"&lat="+latitude.value+"&cloud_score="+cloud.checked+"&api_key=H3LLqaHsYFliawvkIED0tmeqfMxcR6ISjTw79is1")
 
-
-  apiRequest3.onload = function () {
-    var data = JSON.parse(this.response)
-
-    if (apiRequest3.status >= 200 && apiRequest3.status < 400) {
-        cloud.checked === true   ? cloud_percentage.innerHTML =  "Cloud : " + data.cloud_score + " %.":  cloud_percentage.innerHTML = "Cloud : Cloud wasn't checked!";
-        earth__image.src = data.url;
-        date__earth.innerHTML = "Date : "+  data.date;
-        planet__earth.innerHTML = "Planet : "+ data.resource.planet;
-             
-      } else {
-        console.log('error earth api request') 
-        date__earth.innerHTML = "API server error";
-
-         
-      }
-     
-
-} 
-
-apiRequest3.send();
-
+fetch('https://api.nasa.gov/planetary/earth/imagery?lon='+longitude.value+'&lat='+latitude.value+'&cloud_score='+cloud.checked+'&api_key=H3LLqaHsYFliawvkIED0tmeqfMxcR6ISjTw79is1')
+  .then((response) => {
+    if(!response.ok) throw new Error(response.status);
+    else return response.json();
+  })
+  .then((data) => { 
+    cloud.checked === true   ? cloud_percentage.innerHTML =  "Cloud : " + data.cloud_score + " %.":  cloud_percentage.innerHTML = "Cloud : Cloud wasn't checked!";
+    earth__image.src = data.url;
+    date__earth.innerHTML = "Date : "+  data.date;
+    planet__earth.innerHTML = "Planet : "+ data.resource.planet;
+  })
+  .catch((error) => {
+    console.log('error: ' + error); 
+  });
 }
-
